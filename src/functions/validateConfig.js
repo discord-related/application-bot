10973f3af9a5020ac163925f3b417344ceca3e79
Danial Raza <danialrazafb@gmail.com>
2023-01-14 05:15:04 +0100

    fix(functions): introduce destructuring

--- src/functions/validateConfig.js
+++ src/functions/validateConfig.js
@@ -1,29 +1,35 @@
 async function validateConfig(config) {
     let resultString = "";
     try {
-        config.token ? "" : resultString += "❌ Token is missing from config.\n";
-        config.applyMessage ? "" : resultString += "❌ Apply message is missing from config.yml\n";
-        config.acceptedColor ? "" : resultString += "❌ Accepted color is missing from config.yml\n";
-        config.pendingColor ? "" : resultString += "❌ Pending color is missing from config.yml\n";
-        config.deniedColor ? "" : resultString += "❌ Denied color is missing from config.yml\n";
-        config.Applications ? (
-            Object.entries(config.Applications).forEach(([name, application]) => {
-                if(application != null) {
-                    application.Questions ? (
-                        Object.entries(application.Questions).forEach(([question, questionOptions]) => {
-                            questionOptions.minLength != undefined ? "" : resultString += `❌ Min length is missing from ${question} in ${name} in config.yml\n`;
-                            questionOptions.maxLength != undefined ? "" : resultString += `❌ Max length is missing from ${question} in ${name} in config.yml\n`;
-                            questionOptions.type != undefined ? "" : resultString += `❌ Type is missing from ${question} in ${name} in config.yml\n`;
-                            questionOptions.required != undefined ? "" : resultString += `❌ Required is missing from ${question} in ${name} in config.yml\n`;
+        const { Applications, acceptedColor, applyMessage, deniedColor, pendingColor, token } = config;
+
+        token ? "" : resultString += "❌ Token is missing from config.\n";
+        applyMessage ? "" : resultString += "❌ Apply message is missing from config.yml\n";
+        acceptedColor ? "" : resultString += "❌ Accepted color is missing from config.yml\n";
+        pendingColor ? "" : resultString += "❌ Pending color is missing from config.yml\n";
+        deniedColor ? "" : resultString += "❌ Denied color is missing from config.yml\n";
+        Applications ? (
+            Object.entries(Applications).forEach(([name, application]) => {
+                const { acceptedRoles, Channel, deniedRoles, description, mentionRoles, Questions, requiredRoles, restrictedRoles } = application;
+
+                if (application != null) {
+                    Questions ? (
+                        Object.entries(Questions).forEach(([question, questionOptions]) => {
+                            const { maxLength, minLength, required, type } = questionOptions;
+
+                            minLength != undefined ? "" : resultString += `❌ Min length is missing from ${question} in ${name} in config.yml\n`;
+                            maxLength != undefined ? "" : resultString += `❌ Max length is missing from ${question} in ${name} in config.yml\n`;
+                            type != undefined ? "" : resultString += `❌ Type is missing from ${question} in ${name} in config.yml\n`;
+                            required != undefined ? "" : resultString += `❌ Required is missing from ${question} in ${name} in config.yml\n`;
                         })
                     ) : resultString += `❌ Questions are missing from ${name} in config.yml\n`;
-                    application.Channel ? "" : resultString += `❌ Channel is missing from ${name} in config.yml\n`;
-                    application.acceptedRoles ? "" : resultString += `❌ Accepted roles are missing from ${name} in config.yml\n`;
-                    application.mentionRoles ? "" : resultString += `❌ Mention roles are missing from ${name} in config.yml\n`;
-                    application.requiredRoles ? "" : resultString += `❌ Required roles are missing from ${name} in config.yml\n`;
-                    application.restrictedRoles ? "" : resultString += `❌ Restricted roles are missing from ${name} in config.yml\n`;
-                    application.deniedRoles ? "" : resultString += `❌ Denied roles are missing from ${name} in config.yml\n`;
-                    application.description ? "" : resultString += `❌ Description is missing from ${name} in config.yml\n`;
+                    Channel ? "" : resultString += `❌ Channel is missing from ${name} in config.yml\n`;
+                    acceptedRoles ? "" : resultString += `❌ Accepted roles are missing from ${name} in config.yml\n`;
+                    mentionRoles ? "" : resultString += `❌ Mention roles are missing from ${name} in config.yml\n`;
+                    requiredRoles ? "" : resultString += `❌ Required roles are missing from ${name} in config.yml\n`;
+                    restrictedRoles ? "" : resultString += `❌ Restricted roles are missing from ${name} in config.yml\n`;
+                    deniedRoles ? "" : resultString += `❌ Denied roles are missing from ${name} in config.yml\n`;
+                    description ? "" : resultString += `❌ Description is missing from ${name} in config.yml\n`;
                 } else {
                     resultString += `❌ Application ${name} is missing data in config.yml\n`;
                 }
@@ -40,5 +46,4 @@ async function validateConfig(config) {
 
 }
 
-
-module.exports = {validateConfig};
+module.exports = { validateConfig };
